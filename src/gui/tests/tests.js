9faa921bda27756f1a884fc4103f5d026e2ec173
eXesaX <exesa@yandex.ru>
2015-07-06 18:00:59 +0500

    made testable enviroment

--- src/gui/tests/tests.js
+++ src/gui/tests/tests.js
@@ -1,3 +1,5 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+var gui = new GUI();
+
+QUnit.test( "Playing Element creating test", function( assert ) {
+  assert.ok( new PlayingElement('test', 'test.png', 0, 0) instanceof PlayingElement, "Passed!" );
 });
